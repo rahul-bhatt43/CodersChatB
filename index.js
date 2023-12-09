e0dfbe9cba4efdd8cc941e2d3ff4960fcd1b1be0
Rahul Bhatt <72143946+rahul-bhatt43@users.noreply.github.com>
2023-12-09 14:17:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const dotenv = require('dotenv')
+const app = express();
+const cors = require('cors')
+app.use(cors());
+dotenv.config();
+
+const server = http.createServer(app);
+
+
+
+const { Server } = require('socket.io')
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        // origin: "https://coderschat.netlify.app",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => {
+    // console.log("User connected", socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+    })
+
+    socket.on("sent_message", (data) => {
+
+        socket.to(data.room).emit("received_message", data);
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send("Coder's Chat Backend")
+})
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running at", process.env.PORT || 3000);
+});
